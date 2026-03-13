d034968a81c8b4b3c19af8a773a08876246b156f
MD Shariful Islam <79306902+mdsharifulislam-r@users.noreply.github.com>
2026-03-13 10:14:41 +0600

    Update MainContainer.tsx

--- components/Login/MainContainer.tsx
+++ components/Login/MainContainer.tsx
@@ -25,7 +25,7 @@ export default function MainContainer() {
       toast.error('Please fill required field')
       return
     }
-    const res = await fetch(`https://back-builder.vercel.app/api/account/12/14/users/login`,{
+    const res = await fetch(`https://back-builder-omega.vercel.app/api/account/12/14/users/login`,{
       method:'POST',
       body:JSON.stringify(formData)
     })
